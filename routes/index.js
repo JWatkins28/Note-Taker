81bc8a883f35b88cf5220f06463ad85a67b37899
JWatkins28 <112731653+JWatkins28@users.noreply.github.com>
2022-12-01 11:05:34 -0600

    Cleaned up some code and added more notes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+// REQUIRE FOR APP
 const express = require('express');
 
 // IMPORTING ROUTES
 const notesRouter = require('./notes');
 
+// SETTING UP EXPRESS
 const app = express();
 
+// INDEX WILL ROUTE TO THE NOTES FILE
 app.use('/notes', notesRouter);
 
 module.exports = app;
